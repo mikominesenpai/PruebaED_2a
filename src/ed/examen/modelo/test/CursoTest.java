33051f096261fd6ec9de63ea3faf5057bc81fc00
Raquel <rachelortiz1999@gmail.com>
2021-02-23 22:48:59 +0100

    arreglo del Commit clase CursoTest

--- src/ed/examen/modelo/test/CursoTest.java
+++ src/ed/examen/modelo/test/CursoTest.java
@@ -7,7 +7,32 @@ import org.junit.jupiter.api.Test;
 class CursoTest {
 
 	@Test
-	final void test() {
+	final void testEliminarAlumno() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testAniadirAlumno() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEstaRegistrado() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCurso() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNumeroAlumnos() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMostrarTodos() {
 		fail("Not yet implemented"); // TODO
 	}
 
