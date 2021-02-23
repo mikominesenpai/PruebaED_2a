6606baadf90f4b4b87f06f0483f7d50957cceda8
Raquel <rachelortiz1999@gmail.com>
2021-02-23 22:47:33 +0100

    Creacion clase CursoTest

---
+++ src/ed/examen/modelo/test/CursoTest.java
@@ -0,0 +1,14 @@
+package ed.examen.modelo.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
