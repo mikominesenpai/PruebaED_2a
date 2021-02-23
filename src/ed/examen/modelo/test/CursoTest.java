b3bfb0b9b7ebff23fd832d44bf28a2988b50aa5e
Raquel <rachelortiz1999@gmail.com>
2021-02-23 22:52:01 +0100

    implementacion testEliminarAlumno()

--- src/ed/examen/modelo/test/CursoTest.java
+++ src/ed/examen/modelo/test/CursoTest.java
@@ -5,14 +5,59 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import ed.examen.modelo.Curso;
+import ed.examen.modelo.Persona;
 
 class CursoTest {
 
 	Curso c= new Curso();
 	
 	@Test
-	final void testEliminarAlumno() {
-		fail("Not yet implemented"); // TODO
+	void testEliminarAlumno() {
+		
+		/*Parametro menor de 9 cifras
+		 * Debe dar error ya que los dni son de 9 cifras*/
+		boolean exceptionLanzada= false;
+		try {
+			c.eliminarAlumno("00");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			exceptionLanzada=true;
+		}
+		assertTrue(exceptionLanzada);
+		
+		
+		/*Parametros de más de 9 cifras*/
+		c.aniadirAlumno(new Persona("123456788","Carlos","Saenz"));
+		/*probamos a eliminar un alumno que no existe*/
+		exceptionLanzada=false;
+		int tamanioBefore=c.numeroAlumnos();
+		try {
+			c.eliminarAlumno("123456789");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int tamanioAfter=c.numeroAlumnos();
+		assertFalse(exceptionLanzada);
+		/*como el alumno no existe, debe ser el mismo tamaño antes que despues*/
+		assertEquals(tamanioBefore, tamanioAfter);
+		
+		/*probamos a eliminar persona que si existe*/
+		exceptionLanzada=false;
+		tamanioBefore=c.numeroAlumnos();
+		try {
+			c.eliminarAlumno("123456788");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tamanioAfter=c.numeroAlumnos();
+		assertFalse(exceptionLanzada);
+		/*como el alumno existia y se ha eliminado, el tamaño de despues debería
+		 * ser el tamaño de antes, es decir el de despues+1*/
+		assertEquals(tamanioBefore, tamanioAfter+1);
+		
+		
 	}
 
 	@Test
