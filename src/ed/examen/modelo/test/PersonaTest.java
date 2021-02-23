82cb9892bf608821e7bef707357c879f4cff96b3
Raquel <rachelortiz1999@gmail.com>
2021-02-23 23:28:54 +0100

    implementacion clase PersonaTest

---
+++ src/ed/examen/modelo/test/PersonaTest.java
@@ -0,0 +1,44 @@
+package ed.examen.modelo.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	final void testPersonaStringStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetDni() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetDni() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetNombre() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetNombre() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetApellido1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetApellido1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
